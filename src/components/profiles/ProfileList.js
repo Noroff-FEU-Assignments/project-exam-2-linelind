e64dde8aad7ee35c9485f8a9981265932cd46edf
linelind <line.toresby@gmail.com>
2022-11-27 14:32:19 +0100

    added search to profile list on feed page

--- src/components/profiles/ProfileList.js
+++ src/components/profiles/ProfileList.js
@@ -8,6 +8,8 @@ import Loader from "../layout/Loader";
 
 export default function ProfileList() {
   const [profiles, setProfiles] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -32,22 +34,54 @@ export default function ProfileList() {
   if (loading) return <Loader />;
   if (error) return <div className='errorMessage'>Oh no, something went wrong.</div>;
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = profiles.filter((profile) => {
+        return profile.name.toLowerCase().includes(searchInput.toLowerCase());
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(profiles);
+    }
+  };
+
   return (
-    <div className='profileListContainer'>
-      {profiles.map((profile) => {
-        return (
-          <Link to={`/profile/${profile.name}`} key={profile.name}>
-            <div className='profileCard'>
-              <div className='avatar avatarSmall'>
-                <img src={profile.avatar ? profile.avatar : FallbackAvatar} alt='Profile avatar.' />
-              </div>
-              <div>
-                <h2>{profile.name}</h2>
-              </div>
-            </div>
-          </Link>
-        );
-      })}
-    </div>
+    <>
+      <div className='searchContainer'>
+        <input className='search' placeholder='Search profile' onChange={(event: any) => searchItems(event.target.value.trim())} />
+      </div>
+      <div className='profileListContainer'>
+        {searchInput.length > 1
+          ? filteredResults.map((profile) => {
+              return (
+                <Link to={`/profile/${profile.name}`} key={profile.name}>
+                  <div className='profileCard'>
+                    <div className='avatar avatarSmall'>
+                      <img src={profile.avatar ? profile.avatar : FallbackAvatar} alt='Profile avatar.' />
+                    </div>
+                    <div>
+                      <h2>{profile.name}</h2>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })
+          : profiles.map((profile) => {
+              return (
+                <Link to={`/profile/${profile.name}`} key={profile.name}>
+                  <div className='profileCard'>
+                    <div className='avatar avatarSmall'>
+                      <img src={profile.avatar ? profile.avatar : FallbackAvatar} alt='Profile avatar.' />
+                    </div>
+                    <div>
+                      <h2>{profile.name}</h2>
+                    </div>
+                  </div>
+                </Link>
+              );
+            })}
+      </div>
+    </>
   );
 }
